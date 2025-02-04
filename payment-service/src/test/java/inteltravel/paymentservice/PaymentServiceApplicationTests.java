b899f4390dda622c9fb759415b36a23a4a0a3ea5
Trần Đức Thịnh <thinhtran9797yb@gmail.com>
2025-02-04 15:01:30 +0700

    init project

---
+++ payment-service/src/test/java/inteltravel/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package inteltravel.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
